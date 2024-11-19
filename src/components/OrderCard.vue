3c02628eae739beee556313ee3f356b18eb8e295
Marleen <marleeneijkemans@gmail.com>
2024-11-19 11:16:34 +0100

    ordercard adjustments

--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -1,37 +1,52 @@
 <template>
-    <div :class="['order-card', order.status.toLowerCase()]">
-      <div class="order-header">
-        <h3>Table {{ order.table }}</h3>
+  <div :class="['order-card', order.status.toLowerCase()]">
+    <div :class="['order-header', getDurationClass()]">
+      <div class="header-info">
+        <p class="tablenr">Table {{ order.table }}</p>
         <span>{{ order.orderId }}</span>
       </div>
-      <div class="order-time">
+      <div class="header-time">
         <span>{{ order.orderTime }}</span>
         <span>{{ order.duration }}</span>
       </div>
-      <ul class="order-items">
-        <li v-for="item in order.items" :key="item.id">
-          <span>{{ item.quantity }}x</span>
-          <span>{{ item.name }}</span>
-          <input 
-            type="checkbox" 
-            :checked="item.completed" 
-            @change="$emit('toggle-item', item.id)" 
-          />
-        </li>
-      </ul>
-      <button @click="$emit('toggle-status')">
-        {{ order.status === 'Open' ? 'Done' : 'Undo' }}
-      </button>
     </div>
-  </template>
-  
-  <script>
-  import "@/styles/OrderCard.css";
-  
-  export default {
-    props: {
-      order: Object
+    <ul class="order-items">
+      <li v-for="item in order.items" :key="item.id">
+        <span>{{ item.quantity }}x</span>
+        <span>{{ item.name }}</span>
+        <input
+            type="checkbox"
+            :checked="item.completed"
+            @change="$emit('toggle-item', item.id)"
+        />
+      </li>
+    </ul>
+    <button class="done-button" @click="$emit('toggle-status')">
+      {{ order.status === 'Open' ? 'Done' : 'Undo' }}
+    </button>
+  </div>
+</template>
+
+
+<script>
+import "@/styles/OrderCard.css";
+
+export default {
+  props: {
+    order: Object
+  },
+  methods: {
+    getDurationClass() {
+      const minutes = parseInt(this.order.duration.match(/\d+/)?.[0] || 0);
+
+      if (minutes < 5) {
+        return "green-header";
+      } else if (minutes >= 5 && minutes < 10) {
+        return "orange-header";
+      } else {
+        return "red-header";
+      }
     }
-  };
-  </script>
-  
+  }
+};
+</script>
